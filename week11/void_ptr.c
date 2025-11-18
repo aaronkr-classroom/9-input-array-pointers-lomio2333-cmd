86785abb795100c5dcdf271ff6f4ed35e248fa55
ththfql <145094239+ththfql@users.noreply.github.com>
2025-11-18 14:10:13 +0900

    week11

---
+++ week11/void_ptr.c
@@ -0,0 +1,9 @@
+﻿//void_ptr.c
+
+#include<stdio.h>
+
+int Getdata(void* p_data, char type) {
+	int result = 0;
+	//type 변수 저장된 값 기준 형 변환 다르게 함
+	if (type == 1) result = *(char*)p_data;//0x78(1바이트)저장
+}
